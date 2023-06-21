c24bc3b0f6b9bd539a2b4ea685c3300ba75c0250
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-21 18:32:42 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,5 +4,7 @@ bind = '127.0.0.1:8000'
 workers = 4
 # gunicorn.conf.py
 
-bind = '192.168.0.100:8000'  # Replace with your desired IP address and port
+
+bind = '52.221.204.49:8000'  # Replace with your development IP address and port
 workers = 4
+
